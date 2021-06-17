4d7c3bf211ae08cd3fd01832182e706a622fa11c
Sheika1404 <rosheika1404@gmail.com>
2021-06-16 20:17:23 -0400

    nested loop method in comments

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 let prefix = "www."
 let pronoun = ["the", "our", "this", "that", "your"];
-let adj = ["great", "big", "adventurous","amused", "enthusiastic"];
+let adj = ["great", "big", "adventurous", "amused", "enthusiastic"];
 let noun = ["jogger", "racoon", "family", "person", "story", "player", "physics"];
 let extension = [".com", ".co", ".io", ".net"]
 
 // function to generate random number
-const getRandomNumber = (max) => Math.floor(Math.random()*max);
+const getRandomNumber = (max) => Math.floor(Math.random() * max);
 
 // function to generate grab that word from the arrays and ling them together
 const generateWebDomain = () =>
-`${prefix}${pronoun[getRandomNumber(pronoun.length)]}${adj[getRandomNumber(adj.length)]}${noun[getRandomNumber(noun.length)]}${extension[getRandomNumber(extension.length)]}`;
-console.log (generateWebDomain());
-  
+  `${prefix}${pronoun[getRandomNumber(pronoun.length)]}${adj[getRandomNumber(adj.length)]}${noun[getRandomNumber(noun.length)]}${extension[getRandomNumber(extension.length)]}`;
+console.log(generateWebDomain());
+
 // function to link the created domains to the html 
 const generateDomain = () => {
-document.querySelector("#domain").innerHTML = generateWebDomain();
+  document.querySelector("#domain").innerHTML = generateWebDomain();
 }
 
 // activate the button to change domains on click `
@@ -25,17 +25,33 @@ generateDomain();
 
 
 
- 
+
 
 // let generateWebDomain = () => {
-//   let genDomain = " ";
+// let prefix = "www."
+// let pronoun = ["the", "our", "this", "that", "your"];
+// let adj = ["great", "big", "adventurous", "amused", "enthusiastic"];
+// let noun = ["jogger", "racoon", "family", "person", "story", "player", "physics"];
+// let extension = [".com", ".co", ".io", ".net"]
+
+//   let genDomain = [];
+
 //   for (let i = 0; i < pronoun.length; i++) {
+
 //     for (let j = 0; j < adj.length; j++) {
+
 //       for (let k = 0; k < noun.length; k++) {
-//         genDomain+="www." + pronoun[i] + adj[j] + noun[k] + ".com" ;
+
+//          for (let l = 0; l < noun.length; l++){
+
+//         genDomain.push(prefix + pronoun[i] + adj[j] + noun[k] + extension[l]);
+
+//          }
 //       }
 //     }
 //   }
-//   document.querySelector("#domain").innerHTML = genDomain();
+// document.querySelector(#domain).innerHTML= genDomain
+
+//   return genDomain;
 // };
- 
+
